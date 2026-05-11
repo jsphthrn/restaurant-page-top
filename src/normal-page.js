c89b97fb5b77a947ea5e9ba3bbebe4926ad32dbd
José Yáñez <jlyp_sn@protonmail.com>
2026-05-10 21:03:02 -0600

    Progress on 'home' tab.

--- src/normal-page.js
+++ src/normal-page.js
@@ -2,7 +2,7 @@ export let localeHome = {
 
     'en': {
 
-        'welcome-message': "Welcome to Kreiger's Burrow!",
+        'welcome-message': "Welcome!",
         'sub-message': "Please make an order by checking the menu and select the desired items.",
         'subheader-dishes': "Popular dishes",
         'subheader-drinks': "Popular drinks",
@@ -14,7 +14,7 @@ export let localeHome = {
 
     'es': {
 
-        'welcome-message': "Bienvenido/a a Kreiger's Burrow!",
+        'welcome-message': "¡Bienvenidos!",
         'sub-message': "Para ordernar, por favor navegar a la pestaña Menú y seleccionar los alimentos deseados.",
         'subheader-dishes': "Platillos populares",
         'subheader-drinks': "Bebidas populares",
@@ -26,7 +26,7 @@ export let localeHome = {
 
     'ja': {
 
-        'welcome-message': "いらっしゃいませ!　クレイガーブラーへようこそ。",
+        'welcome-message': "いらっしゃいませ!",
         'sub-message': "Please make an order by checking the menu and select the desired items.",
         'subheader-dishes': "人気な食べ物",
         'subheader-drinks': "人気な飲み物",
@@ -38,7 +38,7 @@ export let localeHome = {
 
     'zh-Hans': {
 
-        'welcome-message': "欢迎可以夹洞穴！",
+        'welcome-message': "欢迎您们！",
         'sub-message': "Please make an order by checking the menu and select the desired items.",
         'subheader-dishes': "有名菜",
         'subheader-drinks': "有名饮料",
@@ -53,18 +53,11 @@ export let localeHome = {
 
 export function deployHome (lang, container) {
 
-    /* let aux = Object.keys(localeHome[lang]);
-
-    for (let i = 0; i < aux.length; i++) {
-
-        
-
-
-    }
-
-    */
-
     const base = document.createElement("div");
+
+    base.setAttribute("id", "base");
+
+    const sectionOne = document.createElement("div");
 
     const welcomeMessage = document.createElement("div");
     welcomeMessage.setAttribute("id", "welcome-message");
@@ -74,9 +67,58 @@ export function deployHome (lang, container) {
     subMessage.setAttribute("id", "sub-message");
     subMessage.textContent = localeHome[lang]["sub-message"];
 
+    sectionOne.appendChild(welcomeMessage);
+    sectionOne.appendChild(subMessage);
+    sectionOne.setAttribute("class","section");
+    sectionOne.setAttribute("id","section-one");
+    
+    const sectionTwo = document.createElement("div");
 
-    base.appendChild(welcomeMessage);
-    base.appendChild(subMessage);
+
+    const subsectionTwoOne = document.createElement("div");
+    subsectionTwoOne.setAttribute("class", "subsection");
+    
+
+    const subsectionTwoTwo = document.createElement("div");
+    subsectionTwoTwo.setAttribute("class", "subsection");
+
+    const subHeaderDishes = document.createElement("div");
+    subHeaderDishes.setAttribute("class","subheader");
+    subHeaderDishes.setAttribute("id","subheader-dishes");
+    subHeaderDishes.textContent = localeHome[lang]["subheader-dishes"];
+
+    subsectionTwoOne.appendChild(subHeaderDishes);
+
+    const elementContainer = document.createElement("div");
+    elementContainer.appendChild(document.createElement("div"));
+    elementContainer.appendChild(document.createElement("div"));
+
+    subsectionTwoOne.appendChild(elementContainer);
+
+    sectionTwo.appendChild(subsectionTwoOne);    
+
+    const subHeaderDrinks = document.createElement("div");
+    subHeaderDrinks.setAttribute("class", "subheader");
+    subHeaderDrinks.setAttribute("id", "subheader-drinks");
+    subHeaderDrinks.textContent = localeHome[lang]["subheader-drinks"];
+
+    subsectionTwoTwo.appendChild(subHeaderDrinks);
+    subsectionTwoTwo.appendChild(elementContainer);
+
+
+    sectionTwo.appendChild(subsectionTwoTwo); 
+    sectionTwo.setAttribute("id","section-two");
+    sectionTwo.setAttribute("class", "section")
+
+    const subHeaderBussiness = document.createElement("div");
+    subHeaderBussiness.setAttribute("class","subheader");
+    subHeaderBussiness.setAttribute("id","subheader-drinks");
+    subHeaderBussiness.textContent = localeHome[lang]["subheader-bussiness"];
+
+
+    base.appendChild(sectionOne);
+    base.appendChild(sectionTwo);
+    base.appendChild(subHeaderBussiness);
 
     return base;
 
